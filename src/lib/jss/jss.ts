5e72cd16088f9735a74847f4f8cd4326ba46c9c8
vulnerabbity <v@example.com>
2022-07-17 05:49:06 +0700

    add jss file

---
+++ src/lib/jss/jss.ts
@@ -0,0 +1,30 @@
+export * from "react-jss"
+
+interface DefineAccessRatioProps {
+  height: number
+  aspectRatio: number
+}
+
+export function defineAspectRatio(props: DefineAccessRatioProps) {
+  const { height, aspectRatio } = props
+
+  const width = height * aspectRatio
+
+  return { height, width }
+}
+
+export const cssConstants = {
+  borderRadius: {
+    small: 6,
+    medium: 12,
+    large: 18,
+  },
+
+  padding: {
+    tiny: 2,
+    smaller: 4,
+    small: 6,
+    medium: 12,
+    large: 18,
+  },
+} as const
