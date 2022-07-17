5c78e9f192dafcbc45c44f91937063183ed9f0bf
vulnerabbity <v@example.com>
2022-07-18 00:19:02 +0700

    add trends and search to home page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,22 +1,42 @@
-import {
-  IonContent,
-  IonHeader,
-  IonPage,
-  IonTitle,
-  IonToolbar,
-} from "@ionic/react"
+import { createUseStyles } from "react-jss"
+import { Link } from "react-router-dom"
+import { SearchbarComponent } from "../components/searchbar/searchbar"
+import { TrendsComponent } from "../components/trends/trends"
 
 import "./Home.scss"
 
 export function Home() {
+  const styles = useStyles()
+
   return (
-    <IonPage>
-      <IonHeader>
-        <IonToolbar>
-          <IonTitle>Blank</IonTitle>
-        </IonToolbar>
-      </IonHeader>
-      <IonContent fullscreen></IonContent>
-    </IonPage>
+    <div className={styles.home}>
+      <header className={styles.header}>
+        <h1>
+          <Link to="/">Plumber</Link>
+        </h1>
+        <div className={styles.searchbar}>
+          <SearchbarComponent></SearchbarComponent>
+        </div>
+        <span></span>
+      </header>
+      <TrendsComponent></TrendsComponent>
+    </div>
   )
 }
+
+const useStyles = createUseStyles({
+  home: {},
+  searchbar: {
+    maxWidth: 700,
+  },
+  header: {
+    color: "black",
+    minHeight: 65,
+
+    display: "flex",
+    justifyContent: "space-between",
+    alignItems: "center",
+
+    padding: "0 25rem",
+  },
+})
