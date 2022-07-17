47256000739865e21826d17ad81ba1a1917cd18f
vulnerabbity <v@example.com>
2022-07-17 20:24:52 +0700

    add video items container

---
+++ src/components/video-items-container/videoItemsContainer.tsx
@@ -0,0 +1,48 @@
+import { createRef } from "react"
+import { createUseStyles } from "react-jss"
+import { IVideo } from "../../lib/interface/Video"
+import { VideoItemVerticalComponent } from "../video-item-vertical/video-item-vertical"
+
+export interface VideoItemsContainerComponentProps {
+  videos: IVideo[]
+}
+
+export function VideoItemsContainerComponent({
+  videos,
+}: VideoItemsContainerComponentProps) {
+  const styles = useStyles()
+
+  return (
+    <div className={styles.host}>
+      <div className={styles.itemsContainer}>
+        {videos.map(video => {
+          return (
+            <div className={styles.item} key={Math.random()}>
+              <VideoItemVerticalComponent
+                video={video}
+              ></VideoItemVerticalComponent>
+            </div>
+          )
+        })}
+      </div>
+    </div>
+  )
+}
+
+const useStyles = createUseStyles({
+  host: {
+    // display: "flex",
+    // justifyContent: "center",
+  },
+  itemsContainer: {
+    display: "grid",
+    // Prefer 300rem for item but make 100vw on screen.width < 300
+    gridTemplateColumns: "repeat(auto-fit, minmax(min(300rem, 100vw), 1fr))",
+    gridGap: 8,
+  },
+  item: {
+    display: "flex",
+    justifyContent: "center",
+    minWidth: "none",
+  },
+})
