061cad26a246e63dee52034c384dabea7f71eb26
vulnerabbity <v@example.com>
2022-07-18 23:18:59 +0700

    add bottom bar playback controll

--- src/components/player/BottomBar.tsx
+++ src/components/player/BottomBar.tsx
@@ -1,27 +1,90 @@
 import { IonButton, IonIcon, IonRange } from "@ionic/react"
 import { createUseStyles } from "react-jss"
 import { scanSharp } from "ionicons/icons"
+import { useEffect, useState } from "react"
+import { cssConstants } from "../../lib/jss/jss"
 
 export interface VideoBottomBarProps {
   onFullscreen?: () => any
+  onCurrentTimeChange?: (change: { currentTime: number }) => any
+  getCurrentTime?: () => number
+  getDuration?: () => number
 }
 
 export function VideoBottomBar(props: VideoBottomBarProps) {
-  const { onFullscreen = () => {} } = props
+  const {
+    getDuration = () => 0,
+    getCurrentTime = () => 0,
+    onFullscreen = () => {},
+    onCurrentTimeChange = () => {},
+  } = props
+
+  const [duration, setDuration] = useState(0)
+  const [currentTime, setCurrentTime] = useState(0)
+
+  const progress = (currentTime / duration) * 100
 
   const styles = useStyles()
+
+  useEffect(() => {
+    sync()
+
+    const interval = setInterval(() => {
+      sync()
+    }, 1000)
+
+    return () => clearInterval(interval)
+  }, [])
+
+  function sync() {
+    updateCurrentTime()
+    updateDuration()
+  }
+
+  function updateDuration() {
+    const duration = getDuration()
+    if (isNaN(duration) === false) {
+      setDuration(duration)
+    }
+  }
+
+  function updateCurrentTime() {
+    const currentTime = getCurrentTime()
+
+    if (isNaN(currentTime) === false) {
+      setCurrentTime(currentTime)
+    }
+  }
 
   return (
     <div className={styles.videoBottomBar}>
       {/* LEFT */}
-      <div>Left</div>
+      <div className={styles.videoBottomBarItem}>
+        <div className={styles.time}>{Math.ceil(currentTime)}</div>
+      </div>
 
-      <div className={styles.spacer}>
-        <IonRange className={styles.ionRange}></IonRange>
+      <div className={`${styles.videoBottomBarItem} ${styles.spacer}`}>
+        <IonRange
+          className={styles.ionRange}
+          value={progress}
+          onIonChange={event => {
+            const newProgress = (event.detail.value as number) / 100
+            const newCurrentTime = newProgress * duration
+
+            // event has class range-pressed if changed manually
+            const isChangedByUser =
+              event.target.classList.contains("range-pressed")
+
+            if (isChangedByUser) {
+              onCurrentTimeChange({ currentTime: newCurrentTime })
+            }
+          }}
+        ></IonRange>
       </div>
 
       {/* RIGHT */}
-      <div>
+      <div className={styles.videoBottomBarItem}>
+        <div className={styles.time}>{Math.ceil(duration)}</div>
         <IonButton
           className={styles.ionicIconButton}
           color="none"
@@ -45,7 +108,18 @@ const useStyles = createUseStyles({
     paddingBottom: 8,
   },
 
+  videoBottomBarItem: {
+    display: "flex",
+    alignItems: "center",
+  },
+
   ionRange: {},
+
+  time: {
+    fontSize: cssConstants.baseFontSize * 1.1,
+    opacity: "1 !important",
+    display: "block !important",
+  },
 
   spacer: {
     flex: "1 1",
