2d6fd061a7f6067baefec34d08bdd18668305310
vulnerabbity <v@example.com>
2022-07-19 17:15:16 +0700

    add player to watch page

--- src/pages/Watch.tsx
+++ src/pages/Watch.tsx
@@ -1,3 +1,50 @@
+import { useEffect, useState } from "react"
+import { VideoPlayerComponent } from "../components/player/player"
+import { adaptPipedApiStreamsToVideoSources } from "../lib/adapters/PipedApiToVideoSource"
+import { ApiExtractor } from "../lib/api-extractor/ApiExtractor"
+import { Fetcher } from "../lib/fetcher/Fetcher"
+import { IDetailedVideo } from "../lib/interface/DetailedVideo"
+import { VideoPlayerSource } from "../lib/interface/VideoPlayerSource"
+
+const fetcher = new Fetcher()
+
 export function WatchPage() {
-  return <>watch</>
+  const [detailedVideo, setDetailedVideo] = useState<IDetailedVideo | null>(
+    null,
+  )
+
+  useEffect(() => {
+    fetchVideo().then(video => {
+      if (!video) {
+        return
+      }
+      setDetailedVideo(video)
+    })
+  }, [])
+
+  return (
+    <div>
+      {detailedVideo && (
+        <VideoPlayerComponent
+          thumbnailUrl={detailedVideo.thumbnailUrl}
+          sources={[
+            {
+              isVideoOnly: false,
+              mimeType: "application/x-mpegURL",
+              url: detailedVideo.hls ?? detailedVideo.dash ?? "",
+              quality: "",
+            },
+          ]}
+        ></VideoPlayerComponent>
+      )}
+    </div>
+  )
+}
+
+async function fetchVideo() {
+  const href = window.location.href
+  const videoId = ApiExtractor.extractVideoIdFromVideoUrl(href)
+
+  const video = await fetcher.fetchDetailedVideo(videoId)
+  return video
 }
