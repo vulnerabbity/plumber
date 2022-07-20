751f988977ac45920bdaf5ac9b6bbe022cbbaf10
vulnerabbity <v@example.com>
2022-07-20 20:10:10 +0700

    add subscriptions reducer

--- src/store/reducers/index.ts
+++ src/store/reducers/index.ts
@@ -1,8 +1,10 @@
 import { combineReducers } from "redux"
+import { subscriptionsReducer } from "./subscriptions"
 import { themeReducer } from "./theme"
 import { trendsReducer } from "./trends"
 
 export const rootReducer = combineReducers({
   trends: trendsReducer,
   theme: themeReducer,
+  subscriptions: subscriptionsReducer,
 })
